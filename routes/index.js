9308f0a89be62536256ba7f8aafa5b8ceda1ed8a
Your Name <you@example.com>
2014-02-14 04:48:33 +0000

    text code for heroku

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,9 @@ exports.view = function(req, res){
   var db = mongojs('test');
   var users = db.collection('users');
   console.log("click on index");
-  users.find({id:1}, function(err, doc) {
+  users.find({}, function(err, doc) {
   	//console.log(doc.length);
+  	console.log(err);
     if(doc!=null && doc.length > 0){
       console.log("db works");
    	  var expLifeSpan = doc[0].ExpLifeSpan;
